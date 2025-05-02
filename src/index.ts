83df25f5aac72032746ff685cb58e608bf94047b
harshahd <hdharsha4@gmail.com>
2025-05-02 13:31:05 +0530

    done packaging

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Icon } from "./components/icon/Icon";
+import type { IconType } from "./components/icon/utils/types";
+import { Button } from "./components/button/Button";
+import { ArrowRight, Bell, ChevronUp} from "./SVGSprite";
+
+export {Icon,IconType,Button,ArrowRight,Bell,ChevronUp};
